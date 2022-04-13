6780b2cd952d4cc3a4e1e052e1d384d63dd719b3
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-04-13 07:33:40 -0700

    A3

---
+++ A3/public/code.js
@@ -0,0 +1,156 @@
+received_data = null;
+
+function resetPage() {
+    $('#unicornNameFilter').prop('checked', false);
+    $('#unicornWieghtFilter').prop('checked', false);
+}
+
+function filter_f() {
+    name_ = "unchecked"
+    weight_ = "unchecked"
+
+    if ($('#unicornNameFilter').is(":checked")) {
+        name_ = "checked"
+    }
+    if ($('#unicornWieghtFilter').is(":checked")) {
+        weight_ = "checked"
+    }
+    console.log(received_data);
+
+    tmp = received_data.map(
+        (ob) => {
+            result = []
+            if (name_ == "checked")
+                result.push(ob["name"])
+
+            if (weight_ == "checked")
+                result.push(ob["weight"])
+
+            return result
+        }
+    )
+    console.log(tmp);
+    $("#result").html("<pre>" + tmp + "</pre>");
+}
+
+
+function process_res(data) {
+    received_data = data;
+    console.log(data)
+
+    result = ""
+
+    for (i = 0; i < data.length; i++) {
+        // for each unicorn
+        result += "<table>"
+        result += "<tr>"
+        // display headers for data field
+        for (field in data[i]) {
+            result += "<th>"
+            result += field
+            result += "</th>"
+
+        }
+        result += "/<tr>"
+        // display information about unicorns
+        result += "<tr>"
+        for (field in data[i]) {
+            result += "<td>"
+            if (field == "loves") {
+                result += "<ul>"
+                for (j = 0; j < data[i]["loves"].length; j++) {
+                    result += "<li>"
+                    result += data[i][field][j]
+                    result += "</li>"
+                }
+                result += "</ul>"
+            } else {
+                result += data[i][field]
+            }
+            result += "</td>"
+
+        }
+        result += "</tr>"
+        result += "</table>"
+
+    }
+    $("#result").html(result)
+    // data = JSON.stringify(JSON.parse(data), null, 4);
+    // $("#result").html("<pre>" + data + "</pre>");
+}
+
+
+function findUnicornByWeight() {
+    console.log("findUnicornByWeight" + "got called!");
+    console.log($("#lowerWeight").val());
+    $.ajax({
+        url: 'https://blooming-anchorage-33176.herokuapp.com/findUnicornByWeight',
+        // url: 'http://localhost:5000/findUnicornByWeight',
+        type: 'POST',
+        data: {
+            "lowerWeight": $("#lowerWeight").val(),
+            "higherWeight": $("#higherWeight").val()
+        },
+        success: process_res
+    })
+    resetPage();
+    $("#filters").show()
+}
+
+
+
+function findUnicornByName() {
+    console.log("findUnicornByName()" + "got called!");
+    console.log($("#unicornName").val())
+
+    $.ajax({
+        url: "https://blooming-anchorage-33176.herokuapp.com/findUnicornByName",
+        // url: 'http://localhost:5000/findUnicornByName',
+        type: "POST",
+        data: {
+            "unicornName": $("#unicornName").val()
+        },
+        success: process_res
+
+    })
+    resetPage();
+    $("#filters").show()
+}
+
+
+function findUnicornByFood() {
+    console.log("findUnicornByFood()" + "got called!");
+    carrotIsChecked = "unchecked"
+    appleIsChecked = "unchecked"
+
+    if ($("#carrot").is(":checked"))
+        carrotIsChecked = "checked"
+
+    if ($("#apple").is(":checked"))
+        appleIsChecked = "checked"
+
+    $.ajax({
+        url: "https://blooming-anchorage-33176.herokuapp.com/findUnicornByFood",
+        // url: 'http://localhost:5000/findUnicornByFood',
+        type: "POST",
+        data: {
+            "appleIsChecked": appleIsChecked,
+            "carrotIsChecked": carrotIsChecked,
+        },
+        success: process_res
+
+    })
+    resetPage();
+    $("#filters").show()
+}
+
+
+function setup() {
+    $("#findUnicornByName").click(findUnicornByName)
+    $("#findUnicornByFood").click(findUnicornByFood)
+    $("#findUnicornByWeight").click(findUnicornByWeight)
+    $("#filter").click(filter_f)
+    $("#filters").hide()
+}
+
+$(document).ready(setup)
